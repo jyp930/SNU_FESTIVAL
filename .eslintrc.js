046f3850566548188ee4df2b0b32de34de33a5de
jyp0808 <jyp0808@loplat.com>
2020-07-31 10:18:12 +0900

    chore: CRA, eslint, craco, alias 설정

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+    },
+    "extends": [
+        "airbnb"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "import/no-unresolved": 0,
+        "comma-dangle": 1,
+        "no-unused-vars": 1
+    },
+    // "settings": {
+    //     "import/resolver": {
+    //         alias: {
+    //             map: [
+    //                 ['/src', '@'],
+    //             ],
+    //             extensions: ['.js', '.json']
+    //         }
+    //     }
+    // }
+};
--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.div`
+  font-size: 120px;
+  font-weight: bold;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>
+        Hello World!
+      </Title>
+      <iframe
+        title="hi"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/M0GP1Dn42m8"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
     </div>
   );
 }
