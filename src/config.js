e5c26fcaeb6e5bfc5beaf110278243c614fee642
jyp0808 <jyp0808@loplat.com>
2020-08-01 00:14:25 +0900

    chore: action with GA_TRACKING_ID

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const GA_TRACKING_KEY = process.env.REACT_APP_GA_TRACKING_ID;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from '@/App';
 import ReactGA from 'react-ga';
+import { GA_TRACKING_KEY } from '@/config';
 
-ReactGA.initialize('UA-174141117-1');
+ReactGA.initialize(GA_TRACKING_KEY);
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
