f9d7055dd0a9c4a525150693d1aa48ebd4df27c8
jyp0808 <jyp0808@loplat.com>
2020-08-01 10:35:12 +0900

    fix: GlobalStyle 추가, eslint-hooks 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,14 @@ module.exports = {
     },
     "extends": [
         "airbnb",
+        "plugin:react-hooks/recommended",
     ],
     "parser": "babel-eslint",
     "rules": {
         "react/jsx-filename-extension": 0,
         "import/no-unresolved": 0,
         "import/no-extraneous-dependencies": 0,
+        "arrow-parens": 0,
         "comma-dangle": 1,
         "no-unused-vars": 1,
         "react/no-array-index-key": 1,
--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Suspense } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import routes from '@/routes';
-import UserAnalytics from '@/UserAnalytics';
+import GaAnalytics from '@/UserAnalytics';
 import { GA_TRACKING_KEY } from '@/config';
+
+import { GlobalStyle } from '@/static/style';
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          { routes.map((route) => (
-            <Route exact key={route.path} path={route.path} component={route.component} />
-          )) }
-        </Switch>
-      </Suspense>
+    <>
+      <GlobalStyle />
 
-      { GA_TRACKING_KEY && <UserAnalytics /> }
-    </Router>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            { routes.map((route) => (
+              <Route exact key={route.path} path={route.path} component={route.component} />
+            )) }
+          </Switch>
+        </Suspense>
+
+        { GA_TRACKING_KEY && <GaAnalytics /> }
+      </Router>
+    </>
   );
 }
 export default App;
