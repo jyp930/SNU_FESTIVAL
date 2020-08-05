4ba3fae73d113c1519c1b19ac793a4c88840fe1a
jyp0808 <jyp0808@loplat.com>
2020-08-05 12:36:28 +0900

    style: 뉴트로 색감 추가

--- src/static/style/index.js
+++ src/static/style/index.js
@@ -5,3 +5,27 @@ export const GlobalStyle = createGlobalStyle`
     // TODO: font
   }
 `;
+
+export const palette = {
+  BLUE_NEWTRO: '#094f8c',
+  DARK_BLUE_NEWTRO: '#023c7f',
+  DARK_BLUE20_NEWTRO: '#013a65',
+  DARK_BLUE30_NEWTRO: '#1e243c',
+  LIGHT_BLUE_NEWTRO: '#0f6eb2',
+  RED_NEWTRO: '#d01725',
+  RED20_NEWTRO: '#cf010c',
+  RED30_NEWTRO: '#b20200',
+  GREEN_NEWTRO: '#2e693b',
+  ORANGE_NEWTRO: '#e05335',
+
+  GRAY_NEWTRO: '#eeeeee',
+  GRAY20_NEWTRO: '#d3c5bb',
+
+  BROWN_NEWTRO: '#b57144',
+  BROWN20_NEWTRO: '#d8a41a',
+
+  WHITE_NEWTRO: '#fff6ed',
+  WHITE20_NEWTRO: '#efe7ce',
+
+  BLACK_NEWTRO: '#1c0b01',
+};
