dc8fffe367a2820c433177d6728abc12f086dc5d
SeongJiChoi <njacob654@gmail.com>
2020-08-19 16:28:30 +0900

    배경+센터로고

--- src/containers/goods/Goods/index.js
+++ src/containers/goods/Goods/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import sal from 'sal.js';
+import mascot from '@I/svg/mascot-pink.svg';
 import * as S from './styles';
 
 function Goods() {
@@ -9,14 +10,17 @@ function Goods() {
   }, []);
 
   return (
-    <div
-      data-sal="slide-up"
-      data-sal-duration="1000"
-      data-sal-delay="2300"
-      data-sal-easing="ease-out-back"
-    >
-      goods
-    </div>
+    <S.StyledGoods>
+      <S.BackgroundImage />
+      <S.GoodsMainImage
+        data-sal="fade"
+        data-sal-duration="2000"
+        data-sal-delay="1000"
+        data-sal-easing="ease-out-back"
+        src={mascot}
+        alt="mascot"
+      />
+    </S.StyledGoods>
   );
 }
 export default Goods;
--- src/containers/goods/Goods/styles.js
+++ src/containers/goods/Goods/styles.js
@@ -1,5 +1,35 @@
 import styled from 'styled-components';
+import { palette, zIndex } from '@S/index';
+import IU3 from '@I/jpeg/IU3.jpeg';
 
 export const StyledGoods = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
 
+export const BackgroundImage = styled.div`
+  position: fixed;
+  background-image: url(${IU3});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  filter: blur(4px);
+`;
+
+export const GoodsMainImage = styled.img`
+  width: 30vw;
+  height: 30vh; 
+  z-index: ${zIndex.base};
+`;
+
+export const GoodsImage = styled.img`
+  
 `;
