185012f936aedb696ee2b5ae9d0a2ac23d18aca2
jyp930 <jyp930@daum.net>
2020-08-27 22:27:52 +0900

    refactor

--- src/foundations/SalFrame/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SalFrame from './index';
-
-export default {
-  title: 'foundations/SalFrame',
-};
-
-export const Default = () => <SalFrame />;
--- src/foundations/SalFrame/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledSalFrame = styled.div`
-  height: 30vh;
-  width: 30vw;
-`;
