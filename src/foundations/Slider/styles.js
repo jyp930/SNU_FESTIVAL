5327dff445850882d8a98145fe486980a3d1e2b1
YNWA8064 <you@example.com>
2020-09-20 00:49:12 +0900

    text svg slider

--- src/foundations/Slider/styles.js
+++ src/foundations/Slider/styles.js
@@ -1,19 +1,26 @@
 import styled from 'styled-components';
 
 export const StyledSlider = styled.div`
-  width: 100%;
-  height: 300px;
-  overscroll-behavior: none;
-
+  width: ${props => props.width * 3}px;
+  text-align: center;
+  display: flex;
+  flex-flow: row;
+  &::-webkit-scrollbar { display: none !important; };
+  -ms-overflow-style: none;
+  scrollbar-width: none;
   -webkit-animation: 5s linear 0s infinite normal slide;
   animation: 5s linear 0s infinite normal slide;
   
   @-webkit-keyframes slide {
-    0% { margin-left: 1%; }
-    100% { margin-left: -100%; }
+    0% { margin-left: 0%; }
+    100% { margin-left: -${props => props.width}px; }
   };
   @keyframes slide {
-    0% { margin-left: 1%; }
-    100% { margin-left: -100%; }
+    0% { margin-left: 0%; }
+    100% { margin-left: -${props => props.width}px; }
   };
+`;
+
+export const SliderContents = styled.img`
+  width: ${props => props.width}px ;
 `;
