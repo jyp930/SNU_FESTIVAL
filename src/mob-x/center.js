db168b87ec53308560c0ac3b22ac88916bb65ea6
jyp0808 <jyp0808@loplat.com>
2020-10-22 12:30:19 +0900

    fix: Observe Hoc to observer

--- src/mob-x/center.js
+++ src/mob-x/center.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action, observable, makeObservable } from 'mobx';
-import { Observer } from 'mobx-react';
+import { observer } from 'mobx-react';
 
 class CenterStore {
   @observable centerComponent = null;
@@ -16,4 +16,4 @@ class CenterStore {
 export const centerStore = new CenterStore();
 
 /** 컴포넌트 */
-export const CenterComponent = () => <Observer>{() => centerStore.centerComponent}</Observer>;
+export const CenterComponent = observer(() => centerStore.centerComponent);
