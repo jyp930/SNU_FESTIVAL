8764315cb7632efef10edcded70dbe0b3a8d9eec
jyp930 <jyp930@daum.net>
2020-10-29 22:12:28 +0900

    feat: 인스타, 페이스북 버튼 추가

--- src/foundations/Footer/index.js
+++ src/foundations/Footer/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import mascot from '@I/svg/mascot/mascot-basic.svg';
+import facebook from '@I/svg/icon/facebook.svg';
+import instagram from '@I/svg/icon/instagram.svg';
 import * as S from './styles';
 
 function Footer() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const openNewSite = useCallback((url) => {
+    window.open(url, '_blank');
+  }, []);
+
   return (
     <S.StyledFooter>
-      <S.Image src={mascot} alt="?" />
+      { isOpen && (
+        <>
+          <S.LinkMenu
+            index={1}
+            onClick={() => openNewSite('https://www.instagram.com/snufestival')}
+          >
+            <S.Image src={instagram} alt="Instagram" />
+          </S.LinkMenu>
+          <S.LinkMenu
+            index={2}
+            onClick={() => openNewSite('https://www.facebook.com/snufestival')}
+          >
+            <S.Image src={facebook} alt="facebook" />
+          </S.LinkMenu>
+        </>
+      )}
+      <S.ExpandButton
+        onClick={() => setIsOpen(state => !state)}
+      >
+        <S.Image src={mascot} alt="?" />
+      </S.ExpandButton>
     </S.StyledFooter>
   );
 }
--- src/foundations/Footer/styles.js
+++ src/foundations/Footer/styles.js
@@ -1,26 +1,59 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { HoverStyle } from '@S/responsive/mouse';
 
-export const StyledFooter = styled.div`
-  width: 4rem;
-  height: 4rem;
+const Menu = css`
+  width: 3rem;
+  height: 3rem;
 
   display: flex;
   align-items: center;
   justify-content: center;
   
-  cursor: pointer;
   background-color: white;
   border-radius: 50%;
-
-  box-shadow: rgba(0, 0, 0, 0.12) 0 3px 5px 2px;
-  transition: box-shadow 0.1s ease-in-out 0s;
   
-  &:hover {
-    box-shadow: rgba(0, 0, 0, 0.12) 1px 1px 10px 10px;
-  }
+  box-shadow: rgba(0, 0, 0, 0.12) 0 3px 5px 2px;
+  ${HoverStyle};
 `;
 
 export const Image = styled.img`
   width: 50%;
   height: 50%;
+`;
+
+export const StyledFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ExpandButton = styled.div`
+  ${Menu};
+  width: 4rem;
+  height: 4rem;
+
+  box-shadow: rgba(0, 0, 0, 0.12) 0 3px 5px 2px;
+  transition: box-shadow 0.1s ease-in-out 0s;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.12) 1px 1px 5px 5px;
+  }
+`;
+
+export const LinkMenu = styled.div`
+  ${Menu};
+  position: absolute;
+  
+  animation-name: slideUp-${({ index }) => index};
+  animation-duration: .5s;
+  animation-fill-mode: forwards;
+
+  @keyframes slideUp-${({ index }) => index} {
+    from { 
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(${({ index }) => -4 * index}rem);
+    }
+  }
 `;
