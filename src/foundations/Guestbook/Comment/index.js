424995375c2ca6f16a8d0b943e4ce06aaa6e3ab0
SeongJiChoi <njacob654@gmail.com>
2020-11-02 00:32:34 +0900

    삭제 popup 레이아웃

--- src/foundations/Guestbook/Comment/index.js
+++ src/foundations/Guestbook/Comment/index.js
@@ -1,22 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PopupModal from '@F/PopupModal';
+import Popup from 'reactjs-popup';
 import * as S from './styles';
 
 function Comment({ items }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const useInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = e => {
+      // console.log(e.target.value);
+      setValue(e.target.value);
+    };
+    return { value, onChange };
+  };
+
+  const Password = useInput('');
 
   return (
     <S.StyledComment>
+      <Popup
+        modal
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        closeOnDocumentClick={false}
+        contentStyle={{
+          display: 'flex', justifyContent: 'center'
+        }}
+      >
+        <S.DeletePopup>
+          <div>삭제하시겠습니까?</div>
+          <S.InputBox placeholder="비밀번호" maxLength="20" {...Password} />
+          <S.ButtonBox>
+            <button onClick={() => {}}>
+              삭제
+            </button>
+            <button onClick={() => setIsModalOpen(false)}>
+              닫기
+            </button>
+          </S.ButtonBox>
+        </S.DeletePopup>
+      </Popup>
       {items.map((item, index) => (
         <S.CommentThread>
-          <PopupModal
-            closeOnDocumentClick={false}
-            isModalOpen={isModalOpen}
-            setIsModalOpen={setIsModalOpen}
-          >
-            popup
-          </PopupModal>
           <S.MainBox>
             <S.ProfileImage src={item.img} />
             <S.ContentsBox>
