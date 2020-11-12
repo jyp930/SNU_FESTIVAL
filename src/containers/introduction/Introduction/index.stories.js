7f4dacf9a8f9dde0b79417e2a3d06be437b8e431
jyp930 <jyp930@daum.net>
2020-11-12 20:02:20 +0900

    fix: 기존 소개페이지 삭제

--- src/containers/introduction/Introduction/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Introduction from './index';
-
-export default {
-  title: 'containers/introduction/Introduction',
-};
-
-export const Default = () => <Introduction />;
