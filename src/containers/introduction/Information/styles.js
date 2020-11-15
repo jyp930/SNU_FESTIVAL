4b084b896135c273e21f1089dbfb4c6c2caa5a53
jyp930 <jyp930@daum.net>
2020-11-15 15:41:14 +0900

    feat: 축제 관련 정보 section UI

--- src/containers/introduction/Information/styles.js
+++ src/containers/introduction/Information/styles.js
@@ -1,18 +1,40 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
 
 export const StyledInformation = styled.div`
   padding-bottom: 5rem;
-  color: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  
+  ${media.lessThan('medium')`
+     flex-direction: column;
+     align-items: center;
+  `};
 `;
 
 export const InfoItem = styled.div`
   width: 20rem;
+  padding: 0 1rem;
+  
+  ${media.lessThan('medium')`
+     width: 80%;
+     margin-bottom: 1rem;
+  `};
 `;
 
 export const InfoTitle = styled.p`
   margin: 0;
-  border-bottom: 1px solid white;
   padding: 1rem 0 ;
+  border-bottom: 1px solid white;
+  ${media.lessThan('medium')`
+    border-top: 1px solid white;
+    border-bottom: none;
+  `};
+  
+  font-weight: bolder;
+  font-size: 1.6rem;
 `;
 
 export const InfoBody = styled.div`
@@ -24,9 +46,12 @@ export const InfoBodyItem = styled.div`
 `;
 
 export const InfoContentTitle = styled.p`
-  
+  font-size: 1.4rem;
+  font-weight: bold;
 `;
 
 export const InfoContent = styled.p`
-  
+  word-break: keep-all;
+  line-height: 1.8;
+  opacity: 0.9;
 `;
