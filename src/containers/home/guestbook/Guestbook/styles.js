2fa9fdd5949f17df9f6010d6405825911124ae7a
jyp930 <jyp930@daum.net>
2020-11-24 21:40:45 +0900

    feat: 메인화면 wave 애니메이션 추가

--- src/containers/home/guestbook/Guestbook/styles.js
+++ src/containers/home/guestbook/Guestbook/styles.js
@@ -9,3 +9,12 @@ export const GuestbookBox = styled.div`
   box-sizing: border-box;
   padding: 0 15vw;
 `;
+
+export const WaveWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  
+  pointer-events: none;
+  opacity: 0.5;
+`;
---
+++ src/foundations/animation/Wave/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Wave from './index';
+
+export default {
+  title: 'foundations/animation/Wave',
+};
+
+export const Default = () => <Wave />;
