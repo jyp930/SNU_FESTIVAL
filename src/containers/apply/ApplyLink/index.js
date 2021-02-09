71253d731a361253240529b7f77d8f66db8e2119
jyp930 <jyp930@daum.net>
2021-02-09 23:39:38 +0900

    fix: 모집 링크 삭제

--- src/containers/apply/ApplyLink/index.js
+++ src/containers/apply/ApplyLink/index.js
@@ -12,18 +12,16 @@ function ApplyLink() {
 
   return (
     <S.StyledApplyLink>
-      <S.Link
-        onClick={openApplyGoogleForm}
-      >
+      <S.Link>
         <Flash
           forever
           duration={2500}
         >
-          https://bit.ly/21봄축하사모집
+          지금은 모집 기간이 아닙니다.
         </Flash>
       </S.Link>
       <S.PeriodText>
-        12월 14일 ~ 12월 23일
+        5월에 새로운 모습으로 찾아뵙겠습니다.
       </S.PeriodText>
     </S.StyledApplyLink>
   );
