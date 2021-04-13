b0832153477375b35d57e25b350cbe5e79f24501
jyp930 <jyp930@daum.net>
2021-04-13 22:23:08 +0900

    fix: 축제 설명 문구 수정

--- src/containers/introduction/ArchiveSection/index.js
+++ src/containers/introduction/ArchiveSection/index.js
@@ -58,13 +58,15 @@ const archives = [
     time: '2018 가을 축제', name: '진리는 나의 빚', image: Poster18Fall, description: '부정할 수 없이 짓눌러오는 빚의 무게에 굴하지 않는, 진리를 빚으로 만들어 내는 사회에 대한 유쾌한 소란',
   },
   {
-    time: '2018 봄 축제', name: '어른이 보호구역', image: Poster18Spring, description: '작은 것 하나에도 큰 행복을 얻고 아무 걱정 없이 뛰어놀던 \'어른이\'들을 위한 축제',
+    time: '2018 봄 축제', name: '어른이 보호구역', image: Poster18Spring, description: '작은 것 하나에도 큰 행복을 얻고 아무 걱정 없이 뛰어놀던 어른이들을 위한 축제',
   },
   {
-    time: '2017 가을 축제', name: '궤도루팡', image: Poster17Fall, description: '여러분이 꾸준히 걸어욌던 "궤도"는 축제가 잠시 훔쳐가겠습니다.',
+    time: '2017 가을 축제', name: '궤도루팡', image: Poster17Fall, description: '여러분이 꾸준히 걸어욌던 궤도는 축제가 잠시 훔쳐가겠습니다.',
   },
   {
-    time: '2017 봄 축제', name: '뭔 나라 이런 나라', image: Poster17Spring,
+    time: '2017 봄 축제', name: '뭔 나라 이런 나라', image: Poster17Spring, description: '지금은 한참 잘못되었다고, 우리가 나아갈 길은 이제부터가 시작이라고.',
   },
-  { time: '2016 가을 축제', name: '쇼윈도 탈출', image: Poster16Fall },
+  {
+    time: '2016 가을 축제', name: '쇼윈도 탈출', image: Poster16Fall, description: '사회에서 요구하는 쇼윈도적인 삶을 탈출하고 휴식처의 공간으로 축제를 즐기자',
+  },
 ];
--- src/containers/introduction/ArchiveSection/styles.js
+++ src/containers/introduction/ArchiveSection/styles.js
@@ -49,14 +49,14 @@ export const Texts = styled.div`
     line-height: 1.6;
 
     &:nth-of-type(1) {
-      font-size: 1.2rem;
+      font-size: 1.5rem;
       font-weight: bold;
       color: transparent;
       -webkit-text-stroke: 1px ${({ theme }) => theme.palette.PURPLE70}
     }
     
     &:nth-of-type(2) {
-      font-size: 2rem;
+      font-size: 2.2rem;
       font-weight: bold;
       color: ${({ theme }) => theme.palette.PURPLE70};
       text-shadow: 0 0 6px rgba(147, 151, 214, 0.45);
