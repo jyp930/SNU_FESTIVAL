3e7eec44ee59f8dee51b291a967a5900bb5b2987
jyp930 <jyp930@daum.net>
2021-04-14 00:04:18 +0900

    feat: 굿즈 페이지 레이아웃 완성

--- src/containers/goods/DisplaySection/index.js
+++ src/containers/goods/DisplaySection/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Window from '@I/goods/window.png';
 import * as S from './styles';
 
 function DisplaySection() {
   return (
     <S.StyledDisplaySection>
-      DisplaySection
+      <S.Item>
+        <S.Window src={Window} alt="창" />
+        <S.Texts>
+          <p>축하사 후드티</p>
+          <p>12000</p>
+        </S.Texts>
+      </S.Item>
+      <S.Item>
+        <S.Window src={Window} alt="창" />
+        <S.Texts>
+          <p>축하사 후드티</p>
+          <p>12000</p>
+        </S.Texts>
+      </S.Item>
+      <S.Item>
+        <S.Window src={Window} alt="창" />
+        <S.Texts>
+          <p>축하사 후드티</p>
+          <p>12000</p>
+        </S.Texts>
+      </S.Item>
+      <S.Item>
+        <S.Window src={Window} alt="창" />
+        <S.Texts>
+          <p>축하사 후드티</p>
+          <p>12000</p>
+        </S.Texts>
+      </S.Item>
     </S.StyledDisplaySection>
   );
 }
