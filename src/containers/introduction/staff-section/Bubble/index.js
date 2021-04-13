ad9021ec18e668ef06d5a61043df4e8a51afc6c3
jyp930 <jyp930@daum.net>
2021-04-13 18:13:37 +0900

    fix: 버블 방울 옵션 수정

--- src/containers/introduction/staff-section/Bubble/index.js
+++ src/containers/introduction/staff-section/Bubble/index.js
@@ -9,22 +9,13 @@ function Bubble({ theme }) {
   return (
     <S.BubbleWrapper>
       <S.Circle
-        r={200}
-        cx={theme.windowWidth * (9 / 10)}
-        cy="-200"
+        r={150}
+        cx={0}
+        cy="-100"
         translate={{ x: Math.random() * 50 - 25, y: 100 + 50 }}
-        fill={rgba(theme.palette.PURPLE50, 0.1)}
+        fill={rgba(theme.palette.PURPLE50, 0.06)}
         index={1}
-        duration={10}
-      />
-      <S.Circle
-        r={30}
-        cx={theme.windowWidth * (7 / 10)}
-        cy="-50"
-        translate={{ x: Math.random() * 50 - 20, y: 100 + 15 }}
-        fill={rgba(theme.palette.PURPLE50, 0.45)}
-        index={2}
-        duration={8}
+        duration={11.5}
       />
       <S.Circle
         r={20}
@@ -32,17 +23,46 @@ function Bubble({ theme }) {
         cy="-100"
         translate={{ x: Math.random() * 50 - 15, y: 100 + 20 }}
         fill={rgba(theme.palette.PURPLE50, 0.64)}
-        index={3}
+        index={5}
         duration={7}
       />
       <S.Circle
-        r={150}
-        cx={0}
+        r={100}
+        cx={theme.windowWidth * (2 / 10)}
         cy="-100"
         translate={{ x: Math.random() * 50 - 25, y: 100 + 50 }}
-        fill={rgba(theme.palette.PURPLE50, 0.06)}
-        index={4}
+        fill={rgba(theme.palette.PURPLE50, 0.2)}
+        index={2}
         duration={13}
+      />
+      <S.Circle
+        r={40}
+        cx={theme.windowWidth * (5 / 10)}
+        cy="-200"
+        translate={{ x: Math.random() * 50 - 25, y: 100 + 50 }}
+        fill="transparent"
+        stroke={rgba(theme.palette.PURPLE50, 0.8)}
+        strokeWidth={1}
+        index={6}
+        duration={8.5}
+      />
+      <S.Circle
+        r={200}
+        cx={theme.windowWidth * (9 / 10)}
+        cy="-200"
+        translate={{ x: Math.random() * 50 - 25, y: 100 + 50 }}
+        fill={rgba(theme.palette.PURPLE50, 0.1)}
+        index={3}
+        duration={10}
+      />
+      <S.Circle
+        r={10}
+        cx={theme.windowWidth * (7 / 10)}
+        cy="-50"
+        translate={{ x: Math.random() * 50 - 20, y: 100 + 15 }}
+        fill={rgba(theme.palette.PURPLE50, 0.65)}
+        index={7}
+        duration={6.5}
       />
       { !isMobile && (
         <>
@@ -51,9 +71,9 @@ function Bubble({ theme }) {
             cx={theme.windowWidth * (5 / 10)}
             cy="-100"
             translate={{ x: Math.random() * 50 - 20, y: 100 + 50 }}
-            fill={rgba(theme.palette.PURPLE50, 0.03)}
-            index={5}
-            duration={11}
+            fill={rgba(theme.palette.PURPLE50, 0.04)}
+            index={4}
+            duration={13}
           />
           <S.Circle
             r={20}
@@ -61,8 +81,8 @@ function Bubble({ theme }) {
             cy="-100"
             translate={{ x: Math.random() * 50 - 15, y: 100 + 20 }}
             fill={rgba(theme.palette.PURPLE50, 0.54)}
-            index={6}
-            duration={7.5}
+            index={8}
+            duration={8.5}
           />
         </>
       )}
