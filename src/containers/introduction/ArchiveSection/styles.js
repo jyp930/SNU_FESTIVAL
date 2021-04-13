fda7bb941710cb284c87cf4eccb03ebd2235cb0e
jyp930 <jyp930@daum.net>
2021-04-13 13:16:47 +0900

    fix: 역대 축제 문구 텍스트 css 수정

--- src/containers/introduction/ArchiveSection/styles.js
+++ src/containers/introduction/ArchiveSection/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import media from 'styled-media-query';
 import { FlexCenterStyle } from '@S/responsive/display';
+import { rgba } from 'polished';
 
 export const Wrapper = styled.div`
   ${FlexCenterStyle};
@@ -40,19 +41,32 @@ export const Poster = styled.div`
 export const Texts = styled.div`
   ${FlexCenterStyle};
   flex-direction: column;
+  margin-top: 1rem;
   
   p {
     color: ${({ theme }) => theme.palette.GRAY90};
     margin: 0;
-    line-height: 2;
+    line-height: 1.6;
 
     &:nth-of-type(1) {
+      font-size: 1.2rem;
       font-weight: bold;
+      color: transparent;
+      -webkit-text-stroke: 1px ${({ theme }) => theme.palette.PURPLE70}
     }
     
     &:nth-of-type(2) {
       font-size: 2rem;
       font-weight: bold;
+      color: ${({ theme }) => theme.palette.PURPLE70};
+      text-shadow: 0 0 6px rgba(147, 151, 214, 0.45);
+    }
+    
+    &:nth-of-type(3) {
+      margin-top: 1rem;
+      font-size: 1rem;
+      font-weight: bold;
+      color: ${({ theme }) => rgba(theme.palette.PURPLE50, 0.45)};
     }
   }
 `;
