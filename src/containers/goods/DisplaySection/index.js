2bf9e06b1a06170c962d3e81769d474c57b5ccb9
jyp930 <jyp930@daum.net>
2021-04-15 22:20:02 +0900

    feat: 굿즈 클릭시 페이지 이동

--- src/containers/goods/DisplaySection/index.js
+++ src/containers/goods/DisplaySection/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Window from '@I/goods/window.png';
+import { useHistory } from 'react-router';
 import * as S from './styles';
 
 function DisplaySection() {
+  const history = useHistory();
+
   const Item = (url, image, name, price) => (
-    <S.Item>
+    <S.Item onClick={() => history.push(`/goods/${url}`)}>
       <S.Window src={image} alt="창" />
       <S.Texts>
         <p>{name}</p>
@@ -16,10 +19,10 @@ function DisplaySection() {
 
   return (
     <S.StyledDisplaySection>
-      {Item('', Window, '축하사 후드티', 12000)}
-      {Item('', Window, '축하사 후드티', 12000)}
-      {Item('', Window, '축하사 후드티', 12000)}
-      {Item('', Window, '축하사 후드티', 12000)}
+      {Item('hood', Window, '축하사 후드티', 12000)}
+      {Item('hood', Window, '축하사 후드티', 12000)}
+      {Item('hood', Window, '축하사 후드티', 12000)}
+      {Item('hood', Window, '축하사 후드티', 12000)}
     </S.StyledDisplaySection>
   );
 }
