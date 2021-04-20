2b01ec45b16eed3ad6bb96f0f1a022233a64614e
jyp930 <jyp930@daum.net>
2021-04-20 20:35:41 +0900

    fix: 관게토 스토리북 살리기

--- src/containers/performance/GameTournament/index.stories.js
+++ src/containers/performance/GameTournament/index.stories.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { theme } from '@S/index';
 import GameTournament from './index';
 
 export default {
   title: 'containers/performance/GameTournament',
 };
 
-export const Default = () => <GameTournament />;
+export const Default = () => (
+  <GameTournament theme={{ windowWidth: 1000, ...theme }} />
+);
