d41f07f5e2ec248929fd431de7ceca43beb169ae
jyp930 <jyp930@daum.net>
2021-04-24 20:20:42 +0900

    feat: 미궁게임 문제 내기 UI

--- src/containers/activity/mini/riddle/AliceTheme/index.js
+++ src/containers/activity/mini/riddle/AliceTheme/index.js
@@ -7,6 +7,14 @@ function AliceTheme() {
   return (
     <S.StyledAliceTheme>
       <S.Background src={AliceBackground} alt="앨리스 배경" />
+      {/* TODO: CONTENT 는 유령의 집이랑 공통 */}
+      <S.Content>
+        <S.Question />
+        <S.Answer>
+          <S.InputBox placeholder="정답을 입력하세요" />
+          <S.Button>등록</S.Button>
+        </S.Answer>
+      </S.Content>
     </S.StyledAliceTheme>
   );
 }
--- src/containers/activity/mini/riddle/AliceTheme/styles.js
+++ src/containers/activity/mini/riddle/AliceTheme/styles.js
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
+import { FlexCenterStyle } from '@S/responsive/display';
+import media from 'styled-media-query';
+import { rgba } from 'polished';
 
 export const StyledAliceTheme = styled.div`
+  position: relative;
   width: 100%;
   height: ${({ theme }) => theme.windowHeight}px;
-`;
-
-export const Background = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+  ${FlexCenterStyle};
   
   @keyframes background {
     from { opacity: 0; }
@@ -20,4 +19,83 @@ export const Background = styled.img`
   animation-fill-mode: forwards;
   animation-duration: 1s;
   animation-delay: 1s;
+`;
+
+export const Background = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const Content = styled.div`
+  ${FlexCenterStyle};
+  flex-direction: column;
+  width: 100%;
+  max-width: 550px;
+  height: 100%;
+  z-index: ${({ theme }) => theme.zIndex.base};
+  
+  ${media.lessThan('medium')`
+    width: 80%;
+  `};
+`;
+
+export const Question = styled.div`
+  ${FlexCenterStyle};
+  background-color: #f5f6ff;
+  width: 100%;
+  height: 100%;
+  max-width: 550px;
+  max-height: 550px;
+  border-radius: 15px;
+  
+  ${media.lessThan('medium')`
+    width: 80vw;
+    height: 80vw;
+  `};
+`;
+
+export const Answer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 2rem;
+  margin-top: 2rem;
+`;
+
+export const InputBox = styled.input`
+  outline: 0;
+  width: 80%;
+  height: 100%;
+  padding: 10px 8px;
+  border-radius: 13px;
+  border: solid 1px #ffffff;
+  background-color: rgba(255, 255, 255, 0.31);
+
+  color: ${({ theme }) => theme.palette.GRAY80};
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+  
+  &::placeholder {
+    color: ${({ theme }) => rgba(theme.palette.GRAY80, 0.4)};
+  }
+  
+  margin-right: 1rem;
+`;
+
+export const Button = styled.div`
+  ${FlexCenterStyle};
+  width: 20%;
+  height: 100%;
+  background-color: white;
+  padding: 10px 0;
+  border-radius: 13px;
+  
+  color: ${({ theme }) => theme.palette.PURPLE70};
+  font-weight: bold;
+  
+  cursor: pointer;
 `;
