86cb0904e6cd52d8094a85bc8196ede992908264
jyp930 <jyp930@daum.net>
2021-05-02 19:11:11 +0900

    fix: 미니게임 룰 style 수정

--- src/containers/activity/mini/home/Rule/styles.js
+++ src/containers/activity/mini/home/Rule/styles.js
@@ -15,11 +15,12 @@ export const Texts = styled.div`
   max-width: 500px;
 
   p {
+    margin: 0;
     color: ${({ theme }) => theme.palette.PURPLE50};
     word-break: keep-all;
   }
   p:nth-of-type(1) {
-    margin: 0 0 1rem;
+    margin: 0 0 0.5rem;
     font-size: 1.3rem;
     font-weight: bold;
     ${media.lessThan('medium')`
@@ -27,12 +28,14 @@ export const Texts = styled.div`
     `};
   }
   p:nth-of-type(2) {
-    margin: 0;
     font-size: 1rem;
     font-weight: 500;
     line-height: 1.6;
     ${media.lessThan('medium')`
       font-size: 0.9rem;
     `};
+  }
+  p:nth-of-type(3) {
+    margin-top: 0.3rem;
   }
 `;
