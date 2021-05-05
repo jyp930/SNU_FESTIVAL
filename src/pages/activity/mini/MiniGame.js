48fe7dd4885c5a775759b7e80c89e0ec2ce3c047
jyp930 <jyp930@daum.net>
2021-05-05 12:07:12 +0900

    fix: 미니게임 문구 수정, preloadImage

--- src/pages/activity/mini/MiniGame.js
+++ src/pages/activity/mini/MiniGame.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MiniGameContainer from '@C/activity/mini/home/MiniGame';
 import Header from '@F/layout/Header';
 import { withTheme } from 'styled-components';
 import PropTypes from 'prop-types';
+import Swirl from '@I/activity/black-and-white/swirl.svg';
+import RedBalloon from '@I/activity/treasure-hunt/balloon-red.png';
+import GreenBalloon from '@I/activity/treasure-hunt/balloon-green.png';
+import PurpleBalloon from '@I/activity/treasure-hunt/balloon-purple.png';
+import AliceOpening from '@I/activity/riddle/alice/alice-opening.png';
+import HauntedHouseOpening from '@I/activity/riddle/hounted-house/haunted-house-opening.png';
+import Polaroid from '@I/activity/home/polaroid.png';
+import MobilePolaroid from '@I/activity/home/polariod-mobile.png';
+import { preloadImage } from '@U/functions/preload';
 
 function MiniGame({ theme }) {
+  useEffect(() => {
+    [RedBalloon, GreenBalloon, PurpleBalloon, AliceOpening, HauntedHouseOpening,
+      Swirl, Polaroid, MobilePolaroid].forEach(preloadImage);
+  }, []);
+
   return (
     <>
       <Header hamburgerColor={theme.palette.PURPLE50} />
