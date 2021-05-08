f1573f9aab34d4d565adfd5dd4b8de5e9016cfc2
jyp930 <jyp930@daum.net>
2021-05-08 21:13:25 +0900

    fix: 모바일 로딩 아이콘 줄이기

--- src/foundations/loading/LoadingMascot/styles.js
+++ src/foundations/loading/LoadingMascot/styles.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
 
 export const StyledLoadingMascot = styled.div`
   width: 100%;
   height: 100%;
   max-width: 140px;
   max-height: 140px;
+
+  ${media.lessThan('medium')`
+    max-width: 100px;
+    max-height: 100px;
+  `};
   
   img {
     width: 100%;
